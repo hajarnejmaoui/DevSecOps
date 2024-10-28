5b3a2a46fd9a27ea6aef4a1e6101aaa60731eba0
Hajar nejmaoui <hajar.nejmaoui3@gmail.com>
2024-10-28 09:59:15 +0100

    Initialisation du projet

---
+++ src/test/java/com/exam/DevSecOps/DevSecOpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exam.DevSecOps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevSecOpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
